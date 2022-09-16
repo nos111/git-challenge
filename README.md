6666ed9c352dad0c660b65d3af79acdc1332733a
Nour Saffour <nsaffour@gmail.com>
2022-09-16 13:14:57 +0200

    added text to readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Challenge
+You can find the text for this challenge in challenge.txt
+
+# Solution 
+The solution is available under solution.txt
+
--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,9 @@
 
 def print_hello():
     print("Hello world")
+
+def print_welcome():
+    print("Welcome to the git challenges")
+
+print_hello()
+print_welcome()
