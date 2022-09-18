88b1bfea69c24f9905d96747335847402be5dfb1
Nour Saffour <nsaffour@gmail.com>
2022-09-18 15:35:12 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+#pull all branches
+git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+git fetch --all
+git pull --all
+
 #setup challenge 01
 git checkout challenge01
 git checkout setup src/
