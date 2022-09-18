41e9c8c967244f385c8d58d0c59a1f352fb986d1
Nour Saffour <nsaffour@gmail.com>
2022-09-18 16:25:12 +0200

    fixed setup order

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read re
 git fetch --all
 git pull --all
 
+#setup challenge05
+git checkout challenge05
+git reset --hard HEAD~1
+
 #setup challenge 01
 git checkout challenge01
 git checkout setup src/
 git restore --staged src/
-
-#setup challenge05
-git checkout challenge05
-git reset --hard HEAD~1
 
 #return to master branch
 git checkout master
